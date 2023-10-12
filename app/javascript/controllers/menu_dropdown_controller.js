a868b7b2c33ed8f9099cdfb5f03fac9cedace7ac
Minquan Tan <minch_tan@hotmail.com>
2023-10-13 04:44:03 +0900

    navbar menu toggle ok

---
+++ app/javascript/controllers/menu_dropdown_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    // this.element.textContent = "Hello World!"
+    // console.log("works or no?")
+    console.log("Hello, Stimulus!", this.element)
+  }
+
+  display() {
+    console.log("please display")
+    const menu = document.querySelector(".dropdown-menu")
+
+    if (menu.classList.contains("hidden")) {
+      menu.classList.remove("hidden")
+    } else {
+      menu.classList.add("hidden")
+    }
+  }
+}

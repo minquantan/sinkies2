49a2420aaf475bd954be388511868ccc14b4b3d4
Minquan Tan <minch_tan@hotmail.com>
2023-10-13 04:27:28 +0900

    hello only works if you connect it retard

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!"
+    // console.log("works or no?")
+    console.log("Hello, Stimulus!", this.element)
   }
 }
